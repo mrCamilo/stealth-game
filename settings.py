a34e6250656f27bdd7e7dde34db5968b9318437a
mrCamilo <mcamilo@protonmail.com>
2020-05-26 05:54:28 -0400

    new class for overall game settings

---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """Stores all settings for stealth game"""
+
+    def __init__(self):
+        ###Initialization
+        self.screen_width = 960
+        self.screen_height = 720
+        self.bg_color = (0, 153, 51)
