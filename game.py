54e534003f3f2268ab03db49158a1c347f253105
mrCamilo <mcamilo@protonmail.com>
2020-05-26 05:37:55 -0400

    changed to classes

--- game.py
+++ game.py
@@ -1,30 +1,29 @@
 import sys
 import pygame
 
-#main function
-def main():
-    
-    #Initialize game
-    pygame.init()
-    pygame.display.set_caption("hey it me")
-    screen = pygame.display.set_mode((480,360))
-    red = (255, 64, 64)
-    screen.fill((red))
+class StealthGame:
+    """Overall Class"""
+
+    def __init__(self):
+        """Initialize game"""
+        pygame.init()
+        pygame.display.set_caption("hey it me")
+        screen = pygame.display.set_mode((480,360))
 
     #Loading image
-    ball = pygame.image.load("images/ball.gif")
-    ballrect = ball.get_rect()
+    #ball = pygame.image.load("images/ball.gif")
+    #ballrect = ball.get_rect()
 
-    running = True
+    def run_game(self):
+        """Main loop"""
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                   sys.exit() 
 
-    #game's main while loop
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-        screen.blit(ball,(0,0))
+        #screen.blit(ball,(0,0))
         pygame.display.flip()
 
-if __name__=="__main__":
-    main()
+if __name__ == "__main__":
+   sg = StealthGame()
+   sg.run_game()
