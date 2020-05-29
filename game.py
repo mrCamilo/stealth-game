b815039ff227e3c3d07e58638c355260aa19e267
mrCamilo <mcamilo@protonmail.com>
2020-05-29 05:17:53 -0400

    move in any direction

--- game.py
+++ game.py
@@ -28,12 +28,26 @@ class StealthGame:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     self.player.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.player.moving_left = True
+                elif event.key == pygame.K_UP:
+                    self.player.moving_up = True
+                elif event.key == pygame.K_DOWN:
+                    self.player.moving_down = True
+
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     self.player.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.player.moving_left = False
+                elif event.key == pygame.K_UP:
+                    self.player.moving_up = False
+                elif event.key == pygame.K_DOWN:
+                    self.player.moving_down = False
 
     def _update_screen(self):
         """Update images on screen, flip to new screen"""
