a6b45e67761e9a060dec464a7f171b4c4ca5cca4
mrCamilo <mcamilo@protonmail.com>
2020-06-01 08:47:57 -0400

    can't go off screen anymore

--- player.py
+++ player.py
@@ -6,12 +6,17 @@ class Player:
     def __init__(self, sg_game):
         """Initialize player and starting position"""
         self.screen = sg_game.screen
+        self.settings = sg_game.settings
         self.screen_rect = sg_game.screen.get_rect()
 
         # Load image and rect
         self.image = pygame.image.load("images/alien.bmp")
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
+
+        self.moving_right = False
+        self.moving_left = False
 
         # Movement flag
         self.moving_right = False
@@ -22,13 +27,19 @@ class Player:
     def update(self):
         """Update ship position based on movement flag."""
         if self.moving_right:
-            self.rect.x +=1
+            self.rect.x += 1 
         if self.moving_left:
-            self.rect.x -=1
+            self.rect.x -= 1
         if self.moving_up:
-            self.rect.y -=1
+            self.rect.y -= 1
         if self.moving_down:
-            self.rect.y +=1
+            self.rect.y += 1
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.player_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.player_speed
+
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw image at current location."""
