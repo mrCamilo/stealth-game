5eed5adaaf3bf30a94d0c89209dd0c89ee77368f
mrCamilo <mcamilo@protonmail.com>
2020-06-01 08:47:46 -0400

    Speed setting

--- settings.py
+++ settings.py
@@ -2,7 +2,10 @@ class Settings:
     """Stores all settings for stealth game"""
 
     def __init__(self):
-        ###Initialization
+        # Initialization
         self.screen_width = 960
         self.screen_height = 720
         self.bg_color = (0, 153, 51)
+
+        # Player setting
+        self.player_speed = 1.0
